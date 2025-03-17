593dc6ec391dc7b04b2baae06fad08e2242c2550
Sid Jain <sid_26@outlook.com>
2025-03-17 22:55:32 +0530

    feat(privy): update credentials

--- apps/frontend/src/lib/env/configs/staging.ts
+++ apps/frontend/src/lib/env/configs/staging.ts
@@ -2,7 +2,7 @@ import type { Config } from '../schema';
 
 const stagingConfig: Config = {
   BACKEND_URL: 'https://localhost:3000',
-  PRIVY_APP_ID: 'cm85vu4bk01t3r17tafv824do',
+  PRIVY_APP_ID: 'cm2lx4u5a03x3rtgp4keapmrb',
   STRIPE_PUBLISHABLE_KEY:
     'pk_test_51R3cu3GBNhxk5AuMfrkHDhbtWWQLOvPn5m3BbhsE4CoqhGPvxxSeCaWOoDnUUbO1JF80kHwXpbogGasguVum9rqG00njdbe5LF',
 };
