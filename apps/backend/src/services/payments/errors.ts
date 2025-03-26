5e634983e0f508db07ac83b68dcc1d0de22a7610
Luis Terrones-Verastegui <luistv.swe@gmail.com>
2025-03-26 09:36:14 -0700

    chore: sort PaymentsService errors alphabetically

--- apps/backend/src/services/payments/errors.ts
+++ apps/backend/src/services/payments/errors.ts
@@ -1,18 +1,18 @@
 import { ServiceError, ServiceName } from '#services/serviceError';
 
-export class NegativeAmountInUsdCentsError extends ServiceError {
-  constructor({ amountInUsdCents }: { amountInUsdCents: number }) {
+export class CreateNewPaymentFailure extends ServiceError {
+  constructor() {
     super(
-      `Received a negative value (${amountInUsdCents.toString()}) for amountInUsdCents`,
+      'Failed to create a new Payment entry in the database',
       ServiceName.PAYMENTS,
     );
   }
 }
 
-export class CreateNewPaymentFailure extends ServiceError {
-  constructor() {
+export class NegativeAmountInUsdCentsError extends ServiceError {
+  constructor({ amountInUsdCents }: { amountInUsdCents: number }) {
     super(
-      'Failed to create a new Payment entry in the database',
+      `Received a negative value (${amountInUsdCents.toString()}) for amountInUsdCents`,
       ServiceName.PAYMENTS,
     );
   }
