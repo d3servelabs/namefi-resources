1c0a6f4724541faad0cdec9ec0852664b102e8f0
samishal1998 <samishal.1998@gmail.com>
2025-03-28 21:59:05 +0200

    feat: add utils for get chains from viem

---
+++ packages/utils/src/chains.ts
@@ -0,0 +1,27 @@
+import { isNotNil, pick, toPairs, values } from 'ramda';
+import * as Chains from 'viem/chains';
+
+/**
+ * Object containing all supported blockchain chains from viem/chains, indexed by name.
+ * @remarks [ The module exports other data, but we only need the chains that's why we filter them out by isNotNil(chain.id) ]
+ */
+export const CHAINS = pick(
+  toPairs(Chains)
+    .filter(([_, chain]) => isNotNil(chain.id))
+    .map(([key]) => key),
+  Chains,
+);
+
+/**
+ * Map of chain objects indexed by their chain ID for efficient lookups.
+ */
+const CHAINS_BY_ID = new Map(values(CHAINS).map((chain) => [chain.id, chain]));
+
+/**
+ * Gets a chain object by its chain ID.
+ * @param chainId - The numeric ID of the blockchain chain to look up
+ * @returns The chain object if found, undefined otherwise
+ */
+export function getChain(chainId: number) {
+  return CHAINS_BY_ID.get(chainId as any);
+}
