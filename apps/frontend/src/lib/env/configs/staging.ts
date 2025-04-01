1425b55aebfc24bddac80fa304b875dc5db2ad6b
Luis Terrones-Verastegui <luistv.swe@gmail.com>
2025-04-01 12:09:48 -0700

    refactor: update the development and staging environment configs stripe key; part of NFI-3305

--- apps/frontend/src/lib/env/configs/staging.ts
+++ apps/frontend/src/lib/env/configs/staging.ts
@@ -4,7 +4,7 @@ const stagingConfig: Config = {
   BACKEND_URL: 'https://localhost:3000',
   PRIVY_APP_ID: 'cm2lx4u5a03x3rtgp4keapmrb',
   STRIPE_PUBLISHABLE_KEY:
-    'pk_test_51QoQjW4Ur0KFhrC0ALEyzZAhYTyNzkR1HgRxr8wuYKd4jeSJbSWuZg7eIcBh0ZIrwRC70zUU4S1bgR3JMAEa4BvF00nnwJVcaw',
+    'pk_test_51Pqc6fP7AJmUlGkqATatN7ovwZrEo0WjmJTjryazMHsXRIzk1WrMQv1C0SQ8J4LrTnrc2O5P4XxnTmtSKIfdl2Ct00o9GOerUj',
 };
 
 export default stagingConfig;
