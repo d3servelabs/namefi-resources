7fea4d31b74fc3f6be8b82891971f782e6e4c8ca
samishal1998 <samishal.1998@gmail.com>
2025-04-04 14:43:29 +0200

    hotfix(esm): use createRequire in bundle-workflows script

--- apps/backend/scripts/bundle-workflows.ts
+++ apps/backend/scripts/bundle-workflows.ts
@@ -1,7 +1,10 @@
 import { existsSync } from 'node:fs';
 import { mkdir, readFile, writeFile } from 'node:fs/promises';
+import { createRequire } from 'node:module';
 import * as path from 'node:path';
 import { bundleWorkflowCode } from '@temporalio/worker';
+
+const require = createRequire(import.meta.url);
 
 async function bundle() {
   const { code } = await bundleWorkflowCode({
