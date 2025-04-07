f5eaa9f4066449aa6137eb458aae174aca3bdff1
samishal1998 <samishal.1998@gmail.com>
2025-04-07 21:31:02 +0200

    feat: add endpoint to get backend config

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -4,10 +4,11 @@ import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 import { logger } from 'hono/logger';
 import { prettyJSON } from 'hono/pretty-json';
-import { config } from './lib/env';
+import { config, secrets } from './lib/env';
 import { nsJsonRouter } from './ns-json';
 import { createContext } from './trpc';
 import { appRouter } from './trpc/routers/appRouter';
+
 const app = new Hono();
 
 app.use(
@@ -39,6 +40,19 @@ app.use(
 );
 
 app.route('v1/ns-json', nsJsonRouter);
+
+app.get('/config', (c) => {
+  const key = c.req.header('x-namefi-key');
+  if (key !== secrets.API_AUTH_KEY) {
+    c.status(401);
+    return c.json({ error: 'Unauthorized' });
+  }
+
+  return c.json({
+    ENVIRONMENT: process.env.ENVIRONMENT,
+    config,
+  });
+});
 
 serve(
   {
