0ad76c833fdfef12f02046e2cb448302bc961e5c
samishal1998 <samishal.1998@gmail.com>
2025-04-07 21:21:34 +0200

    hotfix: fix BACKEND_URL in frontend config

--- apps/frontend/src/lib/env/configs/production.ts
+++ apps/frontend/src/lib/env/configs/production.ts
@@ -1,7 +1,7 @@
 import type { Config } from '../schema';
 
 const productionConfig: Config = {
-  BACKEND_URL: 'https://backend.astra.namefi.dev',
+  BACKEND_URL: 'https://backend.astra.namefi.io',
   PRIVY_APP_ID: 'cm2lx4u5a03x3rtgp4keapmrb',
   STRIPE_PUBLISHABLE_KEY:
     'pk_test_51Pqc6fP7AJmUlGkqATatN7ovwZrEo0WjmJTjryazMHsXRIzk1WrMQv1C0SQ8J4LrTnrc2O5P4XxnTmtSKIfdl2Ct00o9GOerUj',
