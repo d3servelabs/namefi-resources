3d52bd67de6ae683600fe6f50d4f8cd752667589
Luis Terrones-Verastegui <luistv.swe@gmail.com>
2025-04-10 17:24:50 -0700

    feature: add GA measurement id to production frontend config

--- apps/frontend/src/lib/env/configs/production.ts
+++ apps/frontend/src/lib/env/configs/production.ts
@@ -2,7 +2,7 @@ import type { Config } from '../schema';
 
 const productionConfig: Config = {
   BACKEND_URL: 'https://backend.astra.namefi.io',
-  GA_MEASUREMENT_ID: 'G-XXXXXXXXXX',
+  GA_MEASUREMENT_ID: 'G-PHKF9PM32W',
   PRIVY_APP_ID: 'cm2lx4u5a03x3rtgp4keapmrb',
   STRIPE_PUBLISHABLE_KEY:
     'pk_test_51Pqc6fP7AJmUlGkqATatN7ovwZrEo0WjmJTjryazMHsXRIzk1WrMQv1C0SQ8J4LrTnrc2O5P4XxnTmtSKIfdl2Ct00o9GOerUj',
