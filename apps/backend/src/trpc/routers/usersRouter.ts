500098047a61391947f937f1f6f15fa2e4a8098f
samishal1998 <samishal.1998@gmail.com>
2025-04-13 22:26:55 +0200

    feat: add an endpoint to get the current authenticated user nfts

--- apps/backend/src/trpc/routers/usersRouter.ts
+++ apps/backend/src/trpc/routers/usersRouter.ts
@@ -1,8 +1,11 @@
 import { db, userUpdateSchema, usersTable } from '@namefi-astra/db';
 import { TRPCError } from '@trpc/server';
 import { eq } from 'drizzle-orm';
+import { isEmpty, isNil } from 'ramda';
 import { z } from 'zod';
+import { resolve } from '../../utils/resolve';
 import { createTRPCRouter, protectedProcedure } from '../base';
+import { privyClient } from '../utils';
 
 export const usersRouter = createTRPCRouter({
   getUser: protectedProcedure.query(async ({ ctx }) => {
@@ -44,4 +47,33 @@ export const usersRouter = createTRPCRouter({
 
       return updatedUser;
     }),
+
+  getCurrentUserDomains: protectedProcedure.query(async ({ ctx }) => {
+    const [error, privyUser] = await resolve(
+      privyClient.getUser({
+        idToken: ctx.user.privyUserId,
+      }),
+    );
+    if (error || isNil(privyUser)) {
+      throw new TRPCError({
+        code: 'PRECONDITION_FAILED',
+        message: 'could not find user details',
+      });
+    }
+
+    // TODO(Sami -> Sid): We need to get the full list of linked wallets from privy
+    const userWalletsAddresses = [privyUser.wallet?.address].filter(
+      (address): address is string => !isNil(address),
+    );
+    if (isEmpty(userWalletsAddresses)) {
+      return [];
+    }
+
+    const nfts = await db.query.namefiNftTable.findMany({
+      where: (table, { inArray }) =>
+        inArray(table.ownerAddress, userWalletsAddresses),
+    });
+
+    return nfts;
+  }),
 });
