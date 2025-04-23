8ef72c08ae4e58df252c10cb0102194e4340b02e
luistv-swe <170651675+luistv-swe@users.noreply.github.com>
2025-04-23 15:16:44 +0000

    feat: update production stripe and google analytics api keys (#384)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai -->
    
    ## Summary by CodeRabbit
    
    - **Chores**
    	- Updated Google Analytics and Stripe configuration to use live production keys.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- apps/frontend/src/lib/env/configs/production.ts
+++ apps/frontend/src/lib/env/configs/production.ts
@@ -2,10 +2,10 @@ import type { ConfigInput } from '../schema';
 
 const productionConfig: ConfigInput = {
   BACKEND_URL: 'https://backend.astra.namefi.io',
-  GA_MEASUREMENT_ID: 'G-PHKF9PM32W',
+  GA_MEASUREMENT_ID: 'G-S8GRYLE2XF',
   PRIVY_APP_ID: 'cm2lx4u5a03x3rtgp4keapmrb',
   STRIPE_PUBLISHABLE_KEY:
-    'pk_test_51Pqc6fP7AJmUlGkqATatN7ovwZrEo0WjmJTjryazMHsXRIzk1WrMQv1C0SQ8J4LrTnrc2O5P4XxnTmtSKIfdl2Ct00o9GOerUj',
+    'pk_live_51Pqc6fP7AJmUlGkq2dbLhK6JAASyzHwYSmlfdHYobkUlJiqeqAkuKykPEb0wTc57n7sqR2QJjI9TDz65Y7zn69I900f95GY1Al',
   NAMEFI_FIRST_PARTY_HOSTNAMES: ['astra.namefi.io'],
   POWERED_BY_NAMEFI_THIRD_PARTY_HOSTNAMES: ['0x.city', 'defi.build'],
   USER_CENTRICS_SETTINGS_ID: '5UJHpI8CWth59m',
