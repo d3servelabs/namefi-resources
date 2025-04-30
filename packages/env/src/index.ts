3a8b375f533fdaa7fe14c0648f321cf39d6b523b
xinbenlv <640325+xinbenlv@users.noreply.github.com>
2025-04-30 00:12:54 +0000

    tooling: add codecov (#524)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai -->
    
    ## Summary by CodeRabbit
    
    - **Chores**
    	- Updated test and coverage scripts for improved reporting and consistency.
    	- Upgraded and reorganized dependencies, including moving some to peer dependencies and updating versions.
    	- Introduced a new configuration file for Codecov to enhance coverage reporting and customize pull request comments.
    	- Improved environment configuration file resolution by explicitly targeting TypeScript files.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -17,7 +17,7 @@ export const loadConfig = <Output, Def extends ZodTypeDef, Input>(
   options: LoadConfigOptions<Output, Def, Input>,
 ) => {
   const validatedBaseConfig = baseConfigSchema.parse(process.env);
-  const envConfigPath = `${options.configPath}/${validatedBaseConfig.ENVIRONMENT}`;
+  const envConfigPath = `${options.configPath}/${validatedBaseConfig.ENVIRONMENT}.ts`;
   let envConfig: unknown;
   try {
     ({ default: envConfig } = require(envConfigPath));
