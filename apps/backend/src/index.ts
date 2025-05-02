ce3772b58c847d3e026529768884053b0a3945e2
samishal1998 <samishal.1998@gmail.com>
2025-05-02 18:12:28 +0300

    hotfix: change configz to configfi since cloudrun reserves paths ending with z

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -67,14 +67,14 @@ app.use(
 app.route('v1/ns-json', nsJsonRouter);
 app.route('/webhooks', webhooksRouter);
 
-app.get('/configz', (c) => {
+app.get('/configfi', (c) => {
   return c.json({
     ENVIRONMENT: process.env.ENVIRONMENT,
     config,
   });
 });
 
-app.get('/secretsz', (c) => {
+app.get('/secretsfi', (c) => {
   const key = c.req.header('x-namefi-key') ?? c.req.query('key');
   if (key !== secrets.API_AUTH_KEY) {
     c.status(401);
