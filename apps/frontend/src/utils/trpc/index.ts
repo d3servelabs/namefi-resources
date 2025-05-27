7f6c0e9e76b8f3a731a3c508892ce11df9cb0f89
samishal1998 <21310077+samishal1998@users.noreply.github.com>
2025-05-27 12:16:45 +0000

    feat(frontend): add infered router types to trpc in the client (#739)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai -->
    ## Summary by CodeRabbit
    
    - **Chores**
      - Added new exported types for improved type inference of inputs and outputs in the app's API.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- apps/frontend/src/utils/trpc/index.ts
+++ apps/frontend/src/utils/trpc/index.ts
@@ -1,7 +1,11 @@
 'use client';
 
 import type { AppRouter } from '@namefi-astra/backend/trpc';
+import type { inferRouterInputs, inferRouterOutputs } from '@trpc/server';
 import { createTRPCContext } from '@trpc/tanstack-react-query';
 
 export const { TRPCProvider, useTRPC, useTRPCClient } =
   createTRPCContext<AppRouter>();
+
+export type AppRouterOutput = inferRouterOutputs<AppRouter>;
+export type AppRouterInput = inferRouterInputs<AppRouter>;
