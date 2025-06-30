d516bc9a0833c44a166c9a77ba64448c5229b106
samishal1998 <samishal.1998@gmail.com>
2025-07-01 00:07:08 +0300

    fix: add missing db migration to add new recordTypes to enum

---
+++ packages/db/src/migrations/0035_last_fixer.sql
@@ -0,0 +1,12 @@
+ALTER TYPE "public"."record_type_enum" ADD VALUE 'NS';--> statement-breakpoint
+ALTER TYPE "public"."record_type_enum" ADD VALUE 'SOA';--> statement-breakpoint
+ALTER TYPE "public"."record_type_enum" ADD VALUE 'PTR';--> statement-breakpoint
+ALTER TYPE "public"."record_type_enum" ADD VALUE 'SRV';--> statement-breakpoint
+ALTER TYPE "public"."record_type_enum" ADD VALUE 'CAA';--> statement-breakpoint
+ALTER TYPE "public"."record_type_enum" ADD VALUE 'DS';--> statement-breakpoint
+ALTER TYPE "public"."record_type_enum" ADD VALUE 'TLSA';--> statement-breakpoint
+ALTER TYPE "public"."record_type_enum" ADD VALUE 'SSHFP';--> statement-breakpoint
+ALTER TYPE "public"."record_type_enum" ADD VALUE 'HTTPS';--> statement-breakpoint
+ALTER TYPE "public"."record_type_enum" ADD VALUE 'SVCB';--> statement-breakpoint
+ALTER TYPE "public"."record_type_enum" ADD VALUE 'NAPTR';--> statement-breakpoint
+ALTER TYPE "public"."record_type_enum" ADD VALUE 'SPF';
