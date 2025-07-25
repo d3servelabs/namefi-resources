5fa719525a5f15d49c11ad0bcfc51068002795af
samishal1998 <samishal.1998@gmail.com>
2025-07-25 13:47:03 +0300

    chore(indexer): reorder some commands in startup script

--- apps/indexer/scripts/startup/footer.sh
+++ apps/indexer/scripts/startup/footer.sh
@@ -1,27 +1,17 @@
 # Start containers
 docker compose up -d
 
-# # Get initial cert
-# docker compose run --rm certbot certonly \
-#   --webroot --webroot-path=/var/www/certbot \
-#   --email $EMAIL --agree-tos --no-eff-email -d $DOMAIN
-
-# docker compose restart nginx
-
-# Add cron job for renewals (skip if already exists)
-(crontab -l 2>/dev/null | grep -q "renew-cert.sh" || \
- (crontab -l 2>/dev/null; echo "30 3 * * * /opt/ponder/renew-cert.sh >> /opt/ponder/renew-cert.log 2>&1")) | crontab -
-
 touch setup-cert.sh
 echo 'DOMAIN='$DOMAIN > setup-cert.sh
+echo 'EMAIL='$EMAIL >> setup-cert.sh
 cat > setup-cert.sh <<'EOF'
 HOST="localhost"
 PORT="80"
 
 echo "Waiting for port $PORT on $HOST to become unavailable..."
 
-# Loop until netcat fails to connect (port is unavailable)
-while nc -z "$HOST" "$PORT"; do
+# Loop until netcat connects (port is unavailable)
+while ! nc -z "$HOST" "$PORT"; do
   echo "Port $PORT is still available. Waiting..."
   sleep 5 # Wait for 1 second before retrying
 done
@@ -36,4 +26,9 @@ docker compose run --rm certbot certonly \
 docker compose restart nginx
 EOF
 
+# Add cron job for renewals (skip if already exists)
+(crontab -l 2>/dev/null | grep -q "renew-cert.sh" || \
+ (crontab -l 2>/dev/null; echo "30 3 * * * /opt/ponder/renew-cert.sh >> /opt/ponder/renew-cert.log 2>&1")) | crontab -
+
+# Make setup-cert.sh executable
 chmod +x ./setup-cert.sh
