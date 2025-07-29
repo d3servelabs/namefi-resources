38bf0689daf3ade49e1a99eb959cbcbc24f8c392
f0rr0 <8574219+f0rr0@users.noreply.github.com>
2025-07-29 22:24:52 +0000

    feat: remove redundant log and todo statement (#1463)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai -->
    
    ## Summary by CodeRabbit
    
    * **Chores**
      * Updated internal documentation for configuration handling.
      * Removed unnecessary debug logging.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -10,14 +10,13 @@ export interface LoadConfigOptions<Output, Def extends ZodTypeDef, Input> {
   configSchema: ZodSchema<Output, Def, Input>;
 }
 
-// TODO(Sami -> Sid): config should be overridable by env vars, otherwise we would have to commit-push-build-deploy every time we want to change a config
+// config is part of the codebase itself, unlike environment variables
 export const loadConfig = <Output, Def extends ZodTypeDef, Input>(
   options: LoadConfigOptions<Output, Def, Input>,
 ) => {
   const validatedBaseConfig = baseConfigSchema.parse(process.env);
   const envConfigPath = `${options.configPath}/${validatedBaseConfig.ENVIRONMENT}`;
   let envConfig: unknown;
-  console.log(envConfigPath);
   try {
     ({ default: envConfig } = require(envConfigPath));
   } catch (_) {
